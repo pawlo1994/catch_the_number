84ee66e57fd8be2ef35a69cca442a29aa12655c6
pawlo4-94 <pawlo4-94@o2.pl>
2022-03-22 17:18:51 +0100

    score, lives and intervalTime added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import { useState } from "react";
 import { AppHeader, GameStartButton, NumberButton, NumberButtonField } from "./styled_app";
+import { GlobalStyle } from "./globalStyle";
 
 const App = () => {
   const [intervalID, setIntervalID] = useState(null);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [x, setX] = useState(10);
   const [y, setY] = useState(10);
+  const [score, setScore] = useState(0);
+  const [lives, setLives] = useState(5);
+  const [intervalTime, setIntervalTime] = useState(1000);
 
   return (
     <>
+      <GlobalStyle />
       <AppHeader>
         Welcome to Jumping Numbers
       </AppHeader>
+      <p>
+        Score: {score}
+      </p>
+      <p>
+        Lives: {+lives}
+      </p>
       <GameStartButton
         onClick={
           () => {
             setIsGameStarted(!isGameStarted);
-            !isGameStarted ?
+            (lives >= 0) ?
               setIntervalID(setInterval(() => {
                 setX(Math.ceil(Math.random() * 200));
                 setY(Math.ceil(Math.random() * 100));
-              }, 1000)) : clearInterval(intervalID);
+                setLives(lives => lives - 1);
+              }, intervalTime)) : clearInterval(intervalID);
           }}
       >
         {!isGameStarted ? "START" : "STOP"}
@@ -35,9 +47,10 @@ const App = () => {
             () => {
               setX(Math.ceil(Math.random() * 200) + 10);
               setY(Math.ceil(Math.random() * 100) + 10);
+              setScore(score + 5);
             }
           }
-          disabled={!isGameStarted}>
+          disabled={!isGameStarted || lives <= 0}>
           5
         </NumberButton>
       </NumberButtonField>
