5773f794a4a8699c006a93e6d58ac1f53da29645
pawlo4-94 <pawlo4-94@o2.pl>
2022-03-24 21:56:04 +0100

    useRef added, intervalID edited

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import { useState } from "react";
-import { GameStartButton, NumberButton, NumberButtonField } from "./styled_app.js";
+import { useRef, useState } from "react";
+import { GameStartButton } from "./styled_app.js";
 import { GlobalStyle } from "./globalStyle.js";
 import { AppHeader } from "./AppHeader";
 import { GameInfo } from "./GameInfo";
+import { NumberButtonField } from "./NumberButtonField/index.js";
 
 const App = () => {
-  const [intervalID, setIntervalID] = useState(null);
   const [isGameStarted, setIsGameStarted] = useState(false);
   const [x, setX] = useState(10);
   const [y, setY] = useState(10);
   const [score, setScore] = useState(0);
   const [lives, setLives] = useState(5);
+  const intervalID = useRef(null);
 
   const setIntervalTime = (intervalTime) => {
-    setIntervalID(setInterval(() => {
+    intervalID.current = setInterval(() => {
       setX(Math.ceil(Math.random() * 200));
       setY(Math.ceil(Math.random() * 100));
       setLives(lives => lives > 0 ? lives - 1 : lives);
-      if (lives === 0) {
-        setIsGameStarted(!isGameStarted);
-        clearInterval(intervalID);
-      }
-    }, intervalTime))
+    }, intervalTime);
+    if (isGameStarted && lives === 0) {
+      clearInterval(intervalID.current);
+    }
   };
 
   return (
     <>
       <GlobalStyle />
-      <AppHeader content="Skaczące cyferki" />
+      <AppHeader content="c4tch th3 numb3r" />
       <GameInfo
         score={score}
         lives={lives}
@@ -35,46 +35,36 @@ const App = () => {
       <GameStartButton
         onClick={
           () => {
-            setLives(5);
-            setScore(0);
-            setIsGameStarted(!isGameStarted);
+            if (lives === 0) {
+              setLives(5);
+              setScore(0);
+            }
+            setIsGameStarted(true);
             if (lives !== 0 && !isGameStarted) {
               setIntervalTime(1000)
             }
-            if (lives === 0) {
-              setIsGameStarted(!isGameStarted);
-              clearInterval(intervalID);
+            else {
+              setIsGameStarted(false)
+              clearInterval(intervalID.current);
             }
           }
         }
+        hidden={isGameStarted && lives !== 0}
       >
-        {!isGameStarted ? "▶️" : "⏹️"}
+        {!isGameStarted ? "▶️" : "⏹"}
       </GameStartButton>
-      <NumberButtonField>
-        <NumberButton
-          style={{
-            left: x > 200 ? x - 40 : x,
-            top: y > 300 ? y - 40 : y,
-          }}
-          onClick={
-            () => {
-              setX(Math.ceil(Math.random() * 200) + 10);
-              setY(Math.ceil(Math.random() * 100) + 10);
-              setScore(score + 5);
-              clearInterval(intervalID);
-              score <= 100 && lives > 0 ?
-                setIntervalTime(1000)
-                : score > 100 && score <= 200 && lives > 0
-                  ? setIntervalTime(500)
-                  : lives > 0
-                    ? setIntervalTime(300)
-                    : clearInterval(intervalID)
-            }
-          }
-          disabled={!isGameStarted || lives <= 0}>
-          5
-        </NumberButton>
-      </NumberButtonField>
+      <NumberButtonField
+        lives={lives}
+        intervalID={intervalID}
+        x={x}
+        setX={setX}
+        y={y}
+        setY={setY}
+        score={score}
+        setScore={setScore}
+        setIntervalTime={setIntervalTime}
+        isGameStarted={isGameStarted}
+      />
     </>
   );
 }
