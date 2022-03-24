137eca1023c4c9367e441466c64deb963b8ced55
pawlo4-94 <pawlo4-94@o2.pl>
2022-03-24 21:57:26 +0100

    styled_app.js edited

--- src/styled_app.js
+++ src/styled_app.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const GameStartButton = styled.button`
     padding: 10px;
@@ -18,37 +18,8 @@ export const GameStartButton = styled.button`
     &:active{
         filter:brightness(1.25);
     }
-`;
 
-export const NumberButtonField = styled.div`
-    position: relative;
-    background-color: #ccc;
-    border-radius: 10px;
-    margin: 15px auto;
-    width: 80%;
-    max-width: 600px;
-    height: 200px;
-`;
-
-export const NumberButton = styled.button`
-    font-size: 60px;
-    padding: 0 20px;
-    background-color: #090;
-    color: #fff;
-    border-radius: 5px;
-    border: none;
-    display: block;
-    position: relative;
-    transition: 0.3s;
-
-    &:active{
-        filter: brightness(1.5);
-        transform: scale(1.2);
-    }
-
-    &:disabled{
-        opacity: 0.2;
-        background-color: #0e0;
-        color: #aaa;
-    }
+    ${({ hidden }) => hidden && css`
+        display: none;
+    `}
 `;
