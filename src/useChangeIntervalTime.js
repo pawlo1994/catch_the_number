9b418be1e6e51bdd7d86bef6512fef1191415c5c
pawlo4-94 <pawlo4-94@o2.pl>
2022-04-01 20:11:10 +0200

    intervalTime selector for custom hook added

--- src/useChangeIntervalTime.js
+++ src/useChangeIntervalTime.js
@@ -1,20 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decreaseLives, selectIsGameStarted, selectLives, setNewNumberButtonPosition, toggleIsGameStarted } from "./gameStatsSlice";
+import { decreaseLives, selectIntervalTime, selectIsGameStarted, selectLives, setNewNumberButtonPosition } from "./gameStatsSlice";
 
-export const useChangeIntervalTime = (intervalID, currentIntervalTime) => {
+export const useChangeIntervalTime = (intervalID) => {
     const dispatch = useDispatch();
     const isGameStarted = useSelector(state => selectIsGameStarted(state));
     const lives = useSelector(state => selectLives(state));
+    const intervalTime = useSelector(state => selectIntervalTime(state));
     const changeIntervalTime = () => {
         intervalID.current = setInterval(() => {
-            dispatch(setNewNumberButtonPosition());
+            dispatch(setNewNumberButtonPosition(lives));
             if (lives !== 0) {
                 dispatch(decreaseLives());
             };
             if (lives === 0) {
                 clearInterval(intervalID.current);
             }
-        }, currentIntervalTime);
+        }, intervalTime);
         if (isGameStarted && lives === 0) {
             clearInterval(intervalID.current);
         }
