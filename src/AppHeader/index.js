4c15a2b1b12f4ebfbce11d29c389c384c92e784e
pawlo4-94 <pawlo4-94@o2.pl>
2022-05-01 22:43:17 +0200

    GameInfo moved into AppHeader

--- src/AppHeader/index.js
+++ src/AppHeader/index.js
@@ -1,9 +1,11 @@
 import { StyledAppHeader, StyledAppHeaderContent } from "./styled.js";
+import { GameInfo } from "./GameInfo";
 
 export const AppHeader = ({ content }) => (
     <StyledAppHeader>
         <StyledAppHeaderContent>
             {content}
         </StyledAppHeaderContent>
+        <GameInfo />
     </StyledAppHeader>
 );
