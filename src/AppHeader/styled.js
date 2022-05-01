8fa7ce65d71d63bb5a875b25fbc06c65a78a267c
pawlo4-94 <pawlo4-94@o2.pl>
2022-05-01 22:42:51 +0200

    styles for styledappheader and styledappheadercontent edited

--- src/AppHeader/styled.js
+++ src/AppHeader/styled.js
@@ -1,14 +1,19 @@
 import styled from "styled-components";
 
 export const StyledAppHeader = styled.header`
-    margin: 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin: 20px;
     padding: 10px 0;
     text-align: center;
     background-color: #aaa;
+    border-radius: 20px;
 `;
 
 export const StyledAppHeaderContent = styled.h1`
-    text-transform: uppercase ;
+    width: 50%;
+    text-transform: uppercase;
     font-size: 40px;
     font-weight: normal;
     text-shadow: 
@@ -17,4 +22,11 @@ export const StyledAppHeaderContent = styled.h1`
      -2px 2px 2px #00f,
      2px -2px 2px #ff0;
     filter: blur(1px);
+    background-color: #ccc;
+    padding: 10px;
+    border-radius: 20px;
+
+    @media(max-width: 767px){
+        font-size: 28px;
+    }
 `
