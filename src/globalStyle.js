f7a70489570126758f004fd4fcd6d49c54ecfab8
pawlo4-94 <pawlo4-94@o2.pl>
2022-05-01 22:42:11 +0200

    page background color edited

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -3,6 +3,6 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     body{
         font-family: "Biryani", sans-serif;
-        background-color: #eee;
+        background-color: #dde;
     }
 `;
