078e57c2d1c75d6a2e9a5c03c7afd07e3cb1b3f7
pawlo4-94 <pawlo4-94@o2.pl>
2022-07-10 13:01:06 +0200

    GameStartButton style edited

--- src/GameStartButton/styled.js
+++ src/GameStartButton/styled.js
@@ -3,12 +3,19 @@ import styled, { css } from "styled-components";
 export const StyledGameStartButton = styled.button`
     padding: 10px;
     transition: ease box-shadow 0.3s;
-    font-size: 20px;
-    display: block;
+    font-size: 32px;
+    width: 48px;
+    height: 48px;
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     margin: 0 auto;
     border: none;
     border-radius: 10px;
     box-shadow: 0px 0px 1px 1px #ccc;
+    background-color: #333;
+    color: #eee;
 
     &:hover{
         cursor: pointer;
