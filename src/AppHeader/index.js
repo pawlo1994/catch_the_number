9844f08560fff8bd23d89626af0a586f3e7b95dd
pawlo4-94 <pawlo4-94@o2.pl>
2022-07-20 21:07:43 +0200

    style for AppHeader edited

--- src/AppHeader/index.js
+++ src/AppHeader/index.js
@@ -1,11 +1,9 @@
 import { StyledAppHeader, StyledAppHeaderContent } from "./styled.js";
-import { GameInfo } from "./GameInfo";
 
 export const AppHeader = ({ content }) => (
     <StyledAppHeader>
         <StyledAppHeaderContent>
             {content}
         </StyledAppHeaderContent>
-        <GameInfo />
     </StyledAppHeader>
 );
--- src/AppHeader/styled.js
+++ src/AppHeader/styled.js
@@ -2,31 +2,22 @@ import styled from "styled-components";
 
 export const StyledAppHeader = styled.header`
     position: relative;
-    margin: 20px auto;
-    width: 80%;
+    margin: 0 auto;
     padding: 10px 0;
     text-align: center;
-    background-color: #aaa;
-    border-radius: 20px;
+    background-color: rgba(150,150,150,0.5);
 `;
 
 export const StyledAppHeaderContent = styled.h1`
     margin: 10px auto;
-    width: 80%;
     text-transform: uppercase;
-    font-size: 40px;
+    letter-spacing: 5px;
+    font-size: 5vw;
     font-weight: normal;
     text-shadow: 
-     2px 2px 2px #f00,
-     -2px -2px 2px #0f0,
-     -2px 2px 2px #00f,
-     2px -2px 2px #ff0;
-    filter: blur(1px);
-    background-color: #ccc;
-    padding: 10px;
-    border-radius: 20px;
-
-    @media(max-width: 767px){
-        font-size: 28px;
-    }
-`
+     2px 2px 4px #eee,
+     -2px -2px 4px #aaa,
+     -2px 2px 4px #ddd,
+     2px -2px 4px #999;
+    padding: 5px;
+`;
