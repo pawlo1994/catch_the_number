44dff05f8081a61104342ecf161829b234b2320b
pawlo4-94 <pawlo4-94@o2.pl>
2022-07-20 21:32:46 +0200

    flex for StyledAppContainer added

--- src/StyledAppContainer/index.js
+++ src/StyledAppContainer/index.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
 
 export const StyledAppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 100vh;
     margin: 0 auto;
     max-width: 1000px;
     width: 90%;
