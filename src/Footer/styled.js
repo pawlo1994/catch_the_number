3915c7d60c7eb6617cbf1a3584ec010e9d62535e
pawlo4-94 <pawlo4-94@o2.pl>
2022-07-23 15:00:26 +0200

    position fixed for footer added

--- src/Footer/styled.js
+++ src/Footer/styled.js
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const StyledFooter = styled.footer`
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
     text-align: center;
     font-size: 18px;
     background-color: #ccc;
