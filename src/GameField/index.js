c4684db44caf40da2a43225e42ba8d1585502962
pawlo4-94 <pawlo4-94@o2.pl>
2022-07-24 14:48:04 +0200

    conditions for dispatching toggleIsGameStarted added

--- src/GameField/index.js
+++ src/GameField/index.js
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import {
     changeCurrentGameState,
     setNewIntervalTime,
-    selectIsGameStarted,
     selectLives,
     selectX,
     selectY,
     selectIntervalTime,
-    selectScore
+    selectScore,
+    toggleIsGameStarted,
+    selectIsGameStarted
 } from "../gameStatsSlice";
 import {
     GameFieldButton,
@@ -22,41 +23,45 @@ export const GameField = ({ intervalID, changeIntervalTime }) => {
     const score = useSelector(state => selectScore(state));
     const isGameStarted = useSelector(state => selectIsGameStarted(state));
     const intervalTime = useSelector(state => selectIntervalTime(state));
+    if (lives === 0 && isGameStarted) {
+        dispatch(toggleIsGameStarted());
+    }
     return (
-        <>
-            <StyledGameField
-                gameOver={lives === 0}
-            >
-                {(lives !== 0)
-                    ?
-                    <GameFieldButton
-                        style={{
-                            transform: `translate(${x}px, ${y}px)`,
-                        }}
-                        onClick={() => {
-                            dispatch(changeCurrentGameState(lives));
-                            clearInterval(intervalID.current);
-                            changeIntervalTime(intervalTime);
-                            if (score === 100) {
-                                dispatch(setNewIntervalTime(800));
-                            }
-                            if (score === 200) {
-                                dispatch(setNewIntervalTime(600));
-                            }
-                            if (score === 300) {
-                                dispatch(setNewIntervalTime(400));
-                            }
-                            if (score === 400) {
-                                dispatch(setNewIntervalTime(200));
-                            }
-                        }}
-                        disabled={!isGameStarted || lives === 0}>
-                        5
-                    </GameFieldButton>
-                    :
-                    `game over. click on restart button, then click on button with number to try again`
-                }
-            </StyledGameField>
-        </>
+        <StyledGameField
+            gameOver={lives === 0}
+        >
+            {(lives !== 0)
+                ?
+                <GameFieldButton
+                    style={{
+                        transform: `translate(${x}px, ${y}px)`,
+                    }}
+                    onClick={() => {
+                        if (!isGameStarted) {
+                            dispatch(toggleIsGameStarted());
+                        }
+                        dispatch(changeCurrentGameState(lives));
+                        clearInterval(intervalID.current);
+                        changeIntervalTime(intervalTime);
+                        if (score === 100) {
+                            dispatch(setNewIntervalTime(800));
+                        }
+                        if (score === 200) {
+                            dispatch(setNewIntervalTime(600));
+                        }
+                        if (score === 300) {
+                            dispatch(setNewIntervalTime(400));
+                        }
+                        if (score === 400) {
+                            dispatch(setNewIntervalTime(200));
+                        }
+                    }}
+                    disabled={!isGameStarted}>
+                    5
+                </GameFieldButton>
+                :
+                `game over. click on restart button to try again`
+            }
+        </StyledGameField>
     )
 };
